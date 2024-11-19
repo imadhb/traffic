63f112a8f3ca7e436b38b8ac554d1a4567f139f5
Imad <imudps@gmail.com>
2024-11-19 12:35:00 -0500

    Created initial setup for the project including frotend and Backend

---
+++ Frontend/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import GoogleMapsScreen from "./GoogleMaps";
+
+export default function App() {
+  return <GoogleMapsScreen />;
+}
---
+++ Frontend/GoogleMaps.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { StyleSheet, View, Dimensions } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
+export default function GoogleMapsScreen() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          title="My Marker"
+          description="This is a marker description"
+        />
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
